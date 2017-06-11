cc110f8dbd40df3ed32b6dff03b54b06e9dc0009
Inayatur Nadhira <inayaturn@gmail.com>
2017-06-12 01:18:34 +0700

    sql ada tidak ada ustadz kalender

---
+++ ustadz_list_jadwal_ada_ustadz_kalender.php
@@ -0,0 +1,33 @@
+<?php
+	require_once './koneksi.php';
+
+    $tanggal = $_POST['tanggal'];
+
+	$query = "SELECT mengajukan.id_ajuan, ustadz.foto_ustadz, ustadz.nama_ustadz, mengajukan.materi, mengajukan.waktu, 
+	mengajukan.tanggal, mengajukan.alamat, mengajukan.status, mengajukan.komentar
+	FROM mengajukan	LEFT JOIN ustadz ON ustadz.id_ustadz = mengajukan.id_ustadz
+	WHERE mengajukan.tanggal ='$tanggal' AND mengajukan.status = 'Terima' ORDER BY mengajukan.waktu DESC";
+			
+	$data_ust = $con->query($query);
+	
+	$hasil = array("tanggal" => []);
+	
+	if($data_ust->num_rows > 0){
+		while($baris = $data_ust->fetch_assoc()){
+			$ubahIndex = array (
+				"nama_ustadz" => $baris["nama_ustadz"],
+                "foto_ustadz" => $baris["foto_ustadz"],
+                "materi" => $baris["materi"],
+                "tanggal" => $baris["tanggal"],
+                "waktu" => $baris["waktu"],
+                "alamat" => $baris["alamat"]
+           
+                );
+                
+            $hasil["tanggal"][] = $ubahIndex;
+		}
+	}
+	
+	
+	echo json_encode($hasil);
+?>
