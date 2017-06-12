33dc4f3c5f6f8cebee49fb614ee910e390cdcd27
Rayhan Yulanda <rayhanyulanda@gmail.com>
2017-06-12 11:45:16 +0700

    perubahan

--- ustadz_list_jadwal_ada_ustadz_kalender.php
+++ ustadz_list_jadwal_ada_ustadz_kalender.php
@@ -3,20 +3,21 @@
 
     $tanggal = $_POST['tanggal'];
 
-	$query = "SELECT mengajukan.id_ajuan, ustadz.foto_ustadz, ustadz.nama_ustadz, mengajukan.materi, mengajukan.waktu, 
+	$query = "SELECT mengajukan.id_ajuan, ustadz.id_ustadz, ustadz.foto_ustadz, ustadz.nama_ustadz, mengajukan.materi, mengajukan.waktu, 
 	mengajukan.tanggal, mengajukan.alamat, mengajukan.status, mengajukan.komentar
 	FROM mengajukan	LEFT JOIN ustadz ON ustadz.id_ustadz = mengajukan.id_ustadz
 	WHERE mengajukan.tanggal ='$tanggal' AND mengajukan.status = 'Terima' ORDER BY mengajukan.waktu DESC";
 			
 	$data_ust = $con->query($query);
 	
-	$hasil = array("tanggal" => []);
+	$hasil = array("ustadzlistsada" => []);
 	
 	if($data_ust->num_rows > 0){
 		while($baris = $data_ust->fetch_assoc()){
 			$ubahIndex = array (
 				"nama_ustadz" => $baris["nama_ustadz"],
                 "foto_ustadz" => $baris["foto_ustadz"],
+                "id_ustadz" => $baris["id_ustadz"],
                 "materi" => $baris["materi"],
                 "tanggal" => $baris["tanggal"],
                 "waktu" => $baris["waktu"],
@@ -24,7 +25,7 @@
            
                 );
                 
-            $hasil["tanggal"][] = $ubahIndex;
+            $hasil["ustadzlistsada"][] = $ubahIndex;
 		}
 	}
 	
